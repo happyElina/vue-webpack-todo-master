7a72d39e7c018fe349455fe21518f1f5e4f19a7f
huxiaodong <1619882712@qq.com>
2019-02-14 11:24:59 +0800

    add editor config

--- client/index.js
+++ client/index.js
@@ -15,4 +15,4 @@ new Vue({
   render: (h) => h(App)
   // vue在创建Vue实例时,通过调用render方法来渲染实例的DOM树,也就是这个组件渲染的是App的内容
   // vue在调用render方法时,会传入一个createElement函数作为参数,也就是这里的h的实参是createElement函数,然后createElement会以App为参数进行调用
-}).$mount(root) // 挂载html的root节点下面
+}).$mount(root) // 挂载html的root节点下面  
